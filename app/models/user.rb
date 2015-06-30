f213558264f5b24402ddfe56a1240772cc37b1ce
bryanelidimas <bryanelidimas@gmail.com>
2015-06-30 16:53:38 -0500

    Add validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :pins
   validates :email, :username, :password, presence: true
+  validates :email, uniqueness: true, length: { minimum: 4 }
+  validates :username, uniqueness: true, length: { minimum: 3 }
+  validates :password, length: { minimum: 5 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX }
 end
